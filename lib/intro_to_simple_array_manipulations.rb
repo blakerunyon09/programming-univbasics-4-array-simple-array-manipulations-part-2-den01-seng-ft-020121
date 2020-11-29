34209df3b5a2da6e9667990079192c3827dc5fdb
Blake Runyon <blakerunyon09@gmail.com>
2020-11-29 17:28:33 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(a1, a2)
+  a1.concat(a2)
+end
+
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_uniq(element)
+  array.uniq(element)
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, element)
+  array.delete(element)
+end
+
+def using_delete_at(array, num)
+  array.delete_at(num)
+end  
